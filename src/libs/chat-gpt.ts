d10fb4c607f444580735e29a4484687c4532639f
Naman Agrawal <naman@innovation.nj.gov>
2024-02-12 16:50:49 -0500

    chore: add ci checks with github actions, fix type issue

--- src/libs/chat-gpt.ts
+++ src/libs/chat-gpt.ts
@@ -1,4 +1,8 @@
-import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
+import {
+  OpenAIClient,
+  AzureKeyCredential,
+  ChatRequestMessage
+} from '@azure/openai';
 
 const ENDPOINT = process.env['AZURE_OPENAI_ENDPOINT'];
 const API_KEY = process.env['AZURE_OPENAI_KEY'];
@@ -25,7 +29,7 @@ export async function getSummary(comments: string[], pageURL: string) {
 
   const systemContent = `You are an assistant designed to find the most common themes in a large dataset of free text. Users will send a list of comments written by residents of New Jersey about their experience applying for ${programName}, where each line represents one comment. You will find the 10 most common themes in the data, and for each theme, you will include a theme title, theme description, and 3 real comments (actually in the dataset, not generated) that fit the given theme. Your output will be in the following structured valid JSON format: {"themes":[{"title":"title 1","description":"description 1","actualComments":["real comment 1","real comment 2","real comment 3"]},{"title":"title 2","description":"description 2","actualComments":["real comment 1","real comment 2","real comment 3"]}, ...]}. Make sure that the 3 comments are in the user-provided list of comments, not generated. Make sure the output is in valid JSON format, and do not add trailing commas.`;
   const userContent = '---\n' + comments.join('\n') + '---';
-  const prompt = [
+  const prompt: ChatRequestMessage[] = [
     {
       role: 'system',
       content: systemContent
