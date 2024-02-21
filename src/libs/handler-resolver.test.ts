e97702bd5df933eeb1606c9e9e07fbcd3c00cb26
Jasmine <jasminee@gmail.com>
2024-02-21 14:25:19 -0500

    combined tests with it.each

--- src/libs/handler-resolver.test.ts
+++ src/libs/handler-resolver.test.ts
@@ -1,6 +1,43 @@
 import { handlerPath } from './handler-resolver';
 
-describe('handlerPath', () => {
+describe('handlerPath success cases', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const testCases = [
+    {
+      name: 'should return a relative UNIX-style path from a UNIX-style absolute path',
+      cwd: '/User/test/feedback-api',
+      inputPath: '/User/test/feedback-api/summary/handler.ts',
+      expected: 'summary/handler.ts'
+    },
+    {
+      name: 'should return a relative UNIX-style path from a Windows-style absolute path',
+      cwd: 'C:\\User\\test\\feedback-api',
+      inputPath: 'C:\\User\\test\\feedback-api\\src\\handler.js',
+      expected: 'src/handler.js'
+    },
+    {
+      name: 'should return an empty string if the input path is the same as cwd path',
+      cwd: '/User/test/feedback-api',
+      inputPath: '/User/test/feedback-api',
+      expected: ''
+    },
+    {
+      name: 'should return an empty string if cwd path is an empty string',
+      cwd: '',
+      inputPath: 'C:\\User\\test\\feedback-api\\src\\handler.js',
+      expected: ''
+    }
+  ];
+  it.each(testCases)('$name', ({ cwd, inputPath, expected }) => {
+    jest.spyOn(process, 'cwd').mockReturnValue(cwd);
+    expect(handlerPath(inputPath)).toBe(expected);
+  });
+});
+
+describe('handlerPath error cases', () => {
   beforeEach(() => {
     jest.spyOn(process, 'cwd').mockReturnValue('/User/test/feedback-api');
   });
@@ -9,35 +46,18 @@ describe('handlerPath', () => {
     jest.resetAllMocks();
   });
 
-  it('should return a relative UNIX-style path from a UNIX-style absolute path', () => {
-    const inputPath = '/User/test/feedback-api/summary/handler.ts';
-    expect(handlerPath(inputPath)).toBe('summary/handler.ts');
-  });
+  const errorTestCases = [
+    {
+      name: 'should throw an error if the context path and cwd path do not share the same starting path',
+      inputPath: '/different/path/not/matching/cwd/handler.ts'
+    },
+    {
+      name: 'should throw an error if the context path is an empty string',
+      inputPath: ''
+    }
+  ];
 
-  it('should return a relative UNIX-style path from a Windows-style absolute path', () => {
-    jest.spyOn(process, 'cwd').mockReturnValue('C:\\User\\test\\feedback-api');
-    const inputPath = 'C:\\User\\test\\feedback-api\\src\\handler.js';
-    expect(handlerPath(inputPath)).toBe('src/handler.js');
-  });
-
-  it('should return an empty string if the input path is the same as cwd path', () => {
-    const inputPath = '/User/test/feedback-api';
-    expect(handlerPath(inputPath)).toBe('');
-  });
-
-  it('should return an empty string if cwd path is an empty string', () => {
-    jest.spyOn(process, 'cwd').mockReturnValue('');
-    const inputPath = 'C:\\User\\test\\feedback-api\\src\\handler.js';
-    expect(handlerPath(inputPath)).toBe('');
-  });
-
-  it('should throw an error if the context path and cwd path do not share the same starting path', () => {
-    const inputPath = '/different/path/not/matching/cwd/handler.ts';
-    expect(() => handlerPath(inputPath)).toThrow(TypeError);
-  });
-
-  it('should throw an error if the context path is an empty string', () => {
-    const inputPath = '';
+  it.each(errorTestCases)('$name', ({ inputPath }) => {
     expect(() => handlerPath(inputPath)).toThrow(TypeError);
   });
 });
