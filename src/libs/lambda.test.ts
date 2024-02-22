4ab6718125abe39db3ed40a57f26a9886e67f252
Jasmine <jasminee@gmail.com>
2024-02-22 15:05:50 -0500

    lambda.ts test

--- src/libs/lambda.test.ts
+++ src/libs/lambda.test.ts
@@ -3,22 +3,20 @@ import middyJsonBodyParser from '@middy/http-json-body-parser';
 import { middyfy } from './lambda';
 
 jest.mock('@middy/core', () => {
-  return jest.fn().mockImplementation(() => {
-    return { use: jest.fn() };
-  });
+  const use = jest.fn().mockImplementation(() => 'middyfied handler');
+  return jest.fn().mockImplementation(() => ({ use }));
 });
-
 jest.mock('@middy/http-json-body-parser');
 
-const mockJsonBodyParser = middyJsonBodyParser as jest.Mock;
-const mockMiddy = middy as jest.Mocked<typeof middy>;
+const mockMiddy = middy as jest.MockedFunction<typeof middy>;
 
 describe('middyfy', () => {
-  it('should wrap handler with middy and applies the JSON body parser', () => {
+  it('should wrap handler with middy, apply JSON body parser, and return middy instance', () => {
     const handler = jest.fn();
-    middyfy(handler);
-    expect(mockMiddy).toHaveBeenCalledWith(handler);
-    expect(mockMiddy).toHaveBeenCalledTimes(1);
-    expect(mockJsonBodyParser).toHaveBeenCalledTimes(1);
+    const middyfiedHandler = middyfy(handler);
+    expect(middy).toHaveBeenCalledWith(handler);
+    const mockMiddyInstance = mockMiddy.mock.results[0].value;
+    expect(mockMiddyInstance.use).toHaveBeenCalledWith(middyJsonBodyParser());
+    expect(middyfiedHandler).toBe('middyfied handler');
   });
 });
