4c8a45b3b4bebefdc9b8189552ab26acf2027497
Jasmine <jasminee@gmail.com>
2024-02-23 11:47:24 -0500

    updated jest config to add env var for tests

---
+++ .jest/envVar.ts
@@ -0,0 +1,4 @@
+process.env['AZURE_OPENAI_ENDPOINT'] = 'test_endpoint'
+process.env['AZURE_OPENAI_KEY'] = 'test_api_key'
+process.env['CLIENT_EMAIL'] = 'test@email.com'
+process.env['GOOGLE_PRIVATE_KEY'] = 'test_private_key'
