2657b1f25d0d7234796a10871f24d2afb84c3edb
Jasmine <jasminee@gmail.com>
2024-02-27 11:22:41 -0500

    removed unnecessary parameter assertion and changed test.each to it.each

--- src/libs/chat-gpt.test.ts
+++ src/libs/chat-gpt.test.ts
@@ -18,13 +18,6 @@ jest.mock('@azure/openai', () => {
 describe('getSummary', () => {
   const comments = ['Test Comment 1', 'Test Comment 2'];
   const userContent = `---\nTest Comment 1\nTest Comment 2---`;
-  const parameters = {
-    maxTokens: 2000,
-    temperature: 0.3,
-    frequencyPenalty: 0,
-    presencePenalty: 0,
-    n: 1
-  };
 
   afterEach(() => {
     jest.clearAllMocks();
@@ -46,7 +39,6 @@ describe('getSummary', () => {
     expect(mockChatCompletionsArgs[0]).toBe(deployment_id);
     expect(mockChatCompletionsArgs[1][0]).toMatchObject(prompt[0]);
     expect(mockChatCompletionsArgs[1][1]).toMatchObject(prompt[1]);
-    expect(mockChatCompletionsArgs[2]).toMatchObject(parameters);
     expect(result).toBe('mocked response');
   });
 
@@ -60,7 +52,7 @@ describe('getSummary', () => {
       ['', 'https://www.example.com']
     ];
 
-    test.each(cases)(
+    it.each(cases)(
       'should utilize a prompt referencing "%s" benefits when page URL is %s',
       async (programName, pageURL) => {
         const systemContent = `You are an assistant designed to find the most common themes in a large dataset of free text. Users will send a list of comments written by residents of New Jersey about their experience applying for ${programName}, where each line represents one comment. You will find the 10 most common themes in the data, and for each theme, you will include a theme title, theme description, and 3 real comments (actually in the dataset, not generated) that fit the given theme. Your output will be in the following structured valid JSON format: {"themes":[{"title":"title 1","description":"description 1","actualComments":["real comment 1","real comment 2","real comment 3"]},{"title":"title 2","description":"description 2","actualComments":["real comment 1","real comment 2","real comment 3"]}, ...]}. Make sure that the 3 comments are in the user-provided list of comments, not generated. Make sure the output is in valid JSON format, and do not add trailing commas.`;
