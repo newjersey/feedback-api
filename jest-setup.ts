c47d770e736b38f03d5fa74fa7f95f44f02bb8dd
Jasmine <jasminee@gmail.com>
2024-02-29 11:28:01 -0500

    added sheetID to jest env var

--- jest-setup.ts
+++ jest-setup.ts
@@ -2,3 +2,4 @@ process.env['AZURE_OPENAI_ENDPOINT'] = 'test_endpoint';
 process.env['AZURE_OPENAI_KEY'] = 'test_api_key';
 process.env['CLIENT_EMAIL'] = 'test@email.com';
 process.env['GOOGLE_PRIVATE_KEY'] = 'test_private_key';
+process.env['SHEET_ID'] = 'test_sheet_id';
