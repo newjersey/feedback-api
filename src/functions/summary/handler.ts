cc0b1e62729882dbbc096bd7e25ea6bad36d9dd1
Jasmine <jasminee@gmail.com>
2024-03-25 12:39:47 -0400

    refactored handler

--- src/functions/summary/handler.ts
+++ src/functions/summary/handler.ts
@@ -3,57 +3,47 @@ import {
   ValidatedEventAPIGatewayProxyEvent
 } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
-import {
-  getAuthClient,
-  getLastNIterative,
-  SHEET_CONFIGS
-} from '@libs/google-sheets';
+import { getAuthClient, getLastNComments } from '@libs/google-sheets';
+import { SHEET_CONFIGS } from '../../constants';
 import { middyfy } from '@libs/lambda';
-
 import schema from './schema';
 import { getSummary } from '@libs/chat-gpt';
+import { resolvedUrl } from '@libs/url-resolver';
 
-const INPUT_SIZE_FREQUENT = 1000;
-const INPUT_SIZE_SPARSE = INPUT_SIZE_FREQUENT * 5;
+
+const INPUT_SIZE = 1000; // Maximum number of comments to summarize
 const summary: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
   const { pageURL } = event.body;
   const sheet =
-    event.body.sheet === 'pflSheet' ? event.body.sheet : 'feedbackWidget';
-  const dataReach = pageURL.includes('uistatus')
-    ? INPUT_SIZE_FREQUENT
-    : INPUT_SIZE_SPARSE;
+    event.body.sheet !== undefined ? event.body.sheet : 'feedbackWidget';
+  const resolvedURL = resolvedUrl(pageURL, sheet);
   try {
     const client = await getAuthClient();
-    const { url, comments } = await getLastNIterative(
+    const comments = await getLastNComments(
       client,
-      dataReach,
-      pageURL,
+      INPUT_SIZE,
+      resolvedURL,
       sheet
     );
-    const filteredData = comments.filter(
-      (v) =>
-        v[SHEET_CONFIGS[sheet].columnOrder.PageURL].includes(url) &&
-        v[SHEET_CONFIGS[sheet].columnOrder.Comment]
-    );
-    if (filteredData.length === 0) {
+    if (comments.length === 0) {
       return formatJSONResponse({
         message: 'No data found',
         dataSize: 0
       });
     }
-    const cleanedComments = filteredData.map((v) =>
+    const cleanedComments = comments.map((v) =>
       v[SHEET_CONFIGS[sheet].columnOrder.Comment].trim()
     );
-    const dataSummary = await getSummary(cleanedComments, pageURL);
+    const dataSummary = await getSummary(cleanedComments, sheet, resolvedURL);
     return formatJSONResponse({
       message: 'Success',
       dataSummary,
-      dataSize: filteredData.length,
-      dataStart: filteredData[0][SHEET_CONFIGS[sheet].columnOrder.Timestamp],
+      dataSize: comments.length,
+      dataStart: comments[0][SHEET_CONFIGS[sheet].columnOrder.Timestamp],
       dataEnd:
-        filteredData[filteredData.length - 1][
+        comments[comments.length - 1][
           SHEET_CONFIGS[sheet].columnOrder.Timestamp
         ]
     });
