cd3a0bbb121d2e9ddbcf6c70d641633fdbf21525
Jasmine <jasminee@gmail.com>
2024-07-19 11:04:17 -0400

    added tab-resolver test

--- src/libs/tab-resolver.ts
+++ src/libs/tab-resolver.ts
@@ -4,8 +4,8 @@ export const determineTabFromUrl = (pageUrl: string) => {
   const filteredTabs = FEEDBACK_SHEET_CONFIG.filteredTabs;
   const allPages = Object.keys(filteredTabs);
   for (const page of allPages) {
-    if (pageUrl.toLowerCase().includes(filteredTabs[page].url)) {
-      return FEEDBACK_SHEET_CONFIG.filteredTabs[page];
+    if (pageUrl.toLowerCase().includes(filteredTabs[page].url.toLowerCase())) {
+      return filteredTabs[page];
     }
   }
   return FEEDBACK_SHEET_CONFIG.defaultPage;
