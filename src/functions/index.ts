3fe9fb7661a955335b8bf55a8bc8197c25aaeff9
sanni <43077388+sannidhishukla@users.noreply.github.com>
2025-06-16 15:47:23 -0600

    Migrate AWS infrastructure from Serverless Framework to AWS CDK (#68)

--- src/functions/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as rating } from './rating';
-export { default as comment } from './comment';
-export { default as email } from './email';
-export { default as summary } from './summary';
