8c240f80effa71f741a86cea861ba60868764af5
sanni <43077388+sannidhishukla@users.noreply.github.com>
2025-06-16 15:47:23 -0600

    Migrate AWS infrastructure from Serverless Framework to AWS CDK (#68)

---
+++ infra/lib/feedback-api-stack.ts
@@ -0,0 +1,105 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
+export class FeedbackApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaExecutionRole = new iam.Role(
+      this,
+      'FeedbackApiLambdaExecutionRole',
+      {
+        roleName: 'FeedbackApiLambdaExecutionRole',
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        description: 'Shared execution role for Feedback API Lambda functions',
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            'service-role/AWSLambdaBasicExecutionRole'
+          )
+        ],
+        inlinePolicies: {
+          feedbackApiLambdaPolicy: new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                  'ssm:GetParameter',
+                  'ssm:GetParameters',
+                  'ssm:GetParametersByPath',
+                  'ssm:DescribeParameters'
+                ],
+                resources: [
+                  `arn:aws:ssm:${this.region}:${this.account}:parameter/feedback-api/*`
+                ]
+              }),
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ['comprehend:DetectPiiEntities'],
+                resources: [`*`]
+              })
+            ]
+          })
+        }
+      }
+    );
+
+    const ratingFunction = new NodejsFunction(this, 'rating', {
+      entry: '../src/functions/rating.ts',
+      functionName: 'feedback-api-rating',
+      role: lambdaExecutionRole,
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: cdk.Duration.seconds(30)
+    });
+
+    const commentFunction = new NodejsFunction(this, 'comment', {
+      entry: '../src/functions/comment.ts',
+      functionName: 'feedback-api-comment',
+      role: lambdaExecutionRole,
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: cdk.Duration.seconds(30)
+    });
+
+    const emailFunction = new NodejsFunction(this, 'email', {
+      entry: '../src/functions/email.ts',
+      functionName: 'feedback-api-email',
+      role: lambdaExecutionRole,
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: cdk.Duration.seconds(30)
+    });
+
+    const functions = [
+      { name: 'rating', handler: ratingFunction, endpointType: 'POST' },
+      { name: 'comment', handler: commentFunction, endpointType: 'POST' },
+      { name: 'email', handler: emailFunction, endpointType: 'POST' }
+    ];
+
+    const feedbackApi = new apigw.RestApi(this, 'feedback-api', {
+      restApiName: 'Feedback API',
+      description: 'API for Feedback Widget functions'
+    });
+
+    functions.forEach(({ name, handler, endpointType }) => {
+      const resource = feedbackApi.root.addResource(name);
+      resource.addMethod(
+        endpointType,
+        new apigw.LambdaIntegration(handler, {})
+      );
+
+      new cdk.CfnOutput(this, `${name}FunctionArnOutput`, {
+        key: `${name}FunctionArn`,
+        exportName: `${name}FunctionArn`,
+        value: handler.functionArn
+      });
+    });
+
+    new cdk.CfnOutput(this, 'ApiUrlOutput', {
+      key: 'feedbackApiUrl',
+      exportName: 'feedbackApiUrl',
+      value: feedbackApi.url
+    });
+  }
+}
---
+++ src/shared/utils/awsUtils.test.ts
@@ -0,0 +1,81 @@
+import * as AwsSdkMockMatchers from 'aws-sdk-client-mock-jest';
+import { mockClient } from 'aws-sdk-client-mock';
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+
+expect.extend(AwsSdkMockMatchers);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getSsmParam } = require('./awsUtils');
+const MOCK_SSM_CLIENT = mockClient(SSMClient);
+
+describe('getSsmParam function', () => {
+  const paramName = 'paramName';
+
+  beforeEach(() => {
+    MOCK_SSM_CLIENT.reset();
+  });
+
+  it('Throws an error if getting SSM parameter fails', async () => {
+    MOCK_SSM_CLIENT.on(GetParameterCommand).rejects('Retrieving failed');
+
+    await expect(getSsmParam(MOCK_SSM_CLIENT, paramName)).rejects.toThrow(
+      /Failed to retrieve parameter.*Retrieving failed/i
+    );
+  });
+
+  it('Returns the parameter value if parameter is successfully retrieved from SSM', async () => {
+    MOCK_SSM_CLIENT.on(GetParameterCommand).resolves({
+      Parameter: {
+        Value: 'paramValue'
+      }
+    });
+
+    const result = await getSsmParam(MOCK_SSM_CLIENT, 'paramName');
+
+    expect(result).toEqual('paramValue');
+  });
+
+  it('Throws an error if the data object is empty', async () => {
+    MOCK_SSM_CLIENT.on(GetParameterCommand).resolves({});
+
+    await expect(getSsmParam(MOCK_SSM_CLIENT, paramName)).rejects.toThrow(
+      expect.objectContaining({
+        message: expect.stringContaining(
+          `Parameter with name ${paramName} does not exist`
+        )
+      })
+    );
+  });
+
+  it('Throws an error if the value of the parameter is null', async () => {
+    MOCK_SSM_CLIENT.on(GetParameterCommand).resolves({
+      Parameter: {
+        Value: null
+      }
+    });
+
+    await expect(getSsmParam(MOCK_SSM_CLIENT, paramName)).rejects.toThrow(
+      expect.objectContaining({
+        message: expect.stringContaining(
+          `Parameter with name ${paramName} does not exist`
+        )
+      })
+    );
+  });
+
+  it('Throws an error if the value of the parameter is empty', async () => {
+    MOCK_SSM_CLIENT.on(GetParameterCommand).resolves({
+      Parameter: {
+        Value: ''
+      }
+    });
+
+    await expect(getSsmParam(MOCK_SSM_CLIENT, paramName)).rejects.toThrow(
+      expect.objectContaining({
+        message: expect.stringContaining(
+          `Parameter with name ${paramName} does not exist`
+        )
+      })
+    );
+  });
+});
