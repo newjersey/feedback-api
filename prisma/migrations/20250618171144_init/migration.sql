d0d5b363f1407c92d5a185d0b87cc0d6ef7c45bf
elizabeth zhang <elizabeth.zhangey@gmail.com>
2025-06-19 12:25:28 -0400

    change createdTime column to timestamptz type

--- prisma/migrations/20250618171144_init/migration.sql
+++ prisma/migrations/20250618171144_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Feedback" (
     "id" SERIAL NOT NULL,
-    "createdTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdTime" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "rating" BOOLEAN NOT NULL,
     "pageUrl" TEXT NOT NULL,
     "comment" TEXT,
