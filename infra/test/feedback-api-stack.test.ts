08321d94bcb594b66e20feeee13797cd4e468d3a
elizabeth zhang <elizabeth.zhangey@gmail.com>
2025-08-20 10:57:49 -0400

    fix feedback-api-stack tests

--- infra/test/feedback-api-stack.test.ts
+++ infra/test/feedback-api-stack.test.ts
@@ -1,47 +1,73 @@
-import { App, Stack } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { App } from 'aws-cdk-lib';
+import { Capture, Template } from 'aws-cdk-lib/assertions';
 import { FeedbackApiStack } from '../lib/feedback-api-stack';
 import path from 'path';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 describe('Feedback API Stack', () => {
-  let app: App;
-  let stack: Stack;
-  let template: Template;
-
-  beforeEach(() => {
-    app = new App();
-
+  const createTemplate = () => {
+    const app = new App();
     const currentWorkingDir = path.basename(path.resolve(process.cwd()));
-    const rootDir = path.resolve(__dirname, '../../');
+    const rootDir = path.basename(path.resolve(__dirname, '../../'));
     if (currentWorkingDir !== rootDir) {
       throw Error(
         `Use the npm scripts in the package.json to make sure you're running tests from the project root, otherwise the pathToSrcDirectory argument used to instantiate the FeedbackApiStack in the beforeEach will be wrong. Currently the working directory is "${currentWorkingDir}". (The project root directory should be: "${rootDir}").`
       );
     }
 
-    stack = new FeedbackApiStack(app, 'TestFeedbackApiStack', {
+    const stack = new FeedbackApiStack(app, 'TestFeedbackApiStack', {
       pathToSrcDirectory: './src'
     });
 
-    template = Template.fromStack(stack);
+    return Template.fromStack(stack);
+  };
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('creates an SNS topic', () => {
+    const template = createTemplate();
     template.hasResourceProperties('AWS::SNS::Topic', {
       TopicName: 'slack-platform-eng-alerts'
     });
   });
 
   it('creates an SNS subscription for Slack endpoint', () => {
+    const valueForStringParameterMock = jest.spyOn(
+      ssm.StringParameter,
+      'valueForStringParameter'
+    );
+    const ssmParamName =
+      '/shared/alarms/subscriptions/slack-emails/platform-eng-alerts';
+
+    const template = createTemplate();
+
+    expect(valueForStringParameterMock).toHaveBeenCalledWith(
+      expect.any(FeedbackApiStack),
+      ssmParamName
+    );
+
+    const endpointCapture = new Capture();
+
     template.hasResourceProperties('AWS::SNS::Subscription', {
       Protocol: 'email',
-      Endpoint: 'platform-eng-alerts-aaaapo5zcsgpp4xexgu4ggjvje@njcio.slack.com'
+      Endpoint: endpointCapture
+    });
+
+    const ssmParamNameAlphanumericOnly = ssmParamName.replace(/\W/g, '');
+
+    expect(endpointCapture.asObject()).toEqual({
+      Ref: expect.stringContaining(
+        `SsmParameterValue${ssmParamNameAlphanumericOnly}`
+      )
     });
   });
 
   it.each(['4XXError', '5XXError'])(
     'creates a CloudWatch alarm tracking the %s metric',
     (metricName) => {
+      const template = createTemplate();
       template.hasResourceProperties('AWS::CloudWatch::Alarm', {
         MetricName: metricName,
         Threshold: 2,
