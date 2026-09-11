24bf181fe5d765416779c48a65a2745196d38169
Sannidhi Shukla <sannidhi@innovation.nj.gov>
2026-09-11 15:44:39 -0500

    remove unused

--- src/functions/rating.test.ts
+++ src/functions/rating.test.ts
@@ -1,8 +1,6 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { describe, expect, it, vi } from 'vitest';
 import { FeedbackResponseStatusCodes } from '../shared/types';
-import * as awsUtils from '../shared/utils/awsUtils';
-import * as googleSheetsUtils from '../shared/utils/googleSheetsUtils';
 import { handler } from './rating';
 
 const TEST_PAGE_URL = 'example.com';
